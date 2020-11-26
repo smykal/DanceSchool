29e4024acaa5a003f6f58a3ba4209e4d8f9af860
mateusz <smykal@engineer.com>
2020-11-26 20:38:50 +0100

    first commit

---
+++ src/test/java/com/danceschool/DanceSchool/DanceSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danceschool.DanceSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanceSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
