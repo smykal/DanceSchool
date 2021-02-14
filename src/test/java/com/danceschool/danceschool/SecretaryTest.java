6b73a70c4b7674ea95ce0ee76e3607e2becfdcae
smykal <smykal@engineer.com>
2021-02-14 21:03:16 +0100

    prepare to @Tests

---
+++ src/test/java/com/danceschool/danceschool/SecretaryTest.java
@@ -0,0 +1,24 @@
+package com.danceschool.danceschool;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SecretaryTest {
+
+    @Test
+    void showGroupMembers() {
+    }
+
+    @Test
+    void changeTeacher() {
+    }
+
+    @Test
+    void addStudent() {
+    }
+
+    @Test
+    void removeStudent() {
+    }
+}
