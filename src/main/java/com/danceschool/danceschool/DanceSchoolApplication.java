4e2eb4364713c0af1dcd701ccaf082bcea057183
Mateusz <smykal@engineer.com>
2021-08-25 21:51:13 +0200

    working connection to database by Hibernate

--- src/main/java/com/danceschool/danceschool/DanceSchoolApplication.java
+++ src/main/java/com/danceschool/danceschool/DanceSchoolApplication.java
@@ -1,12 +1,13 @@
 package com.danceschool.danceschool;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 
 @SpringBootApplication
 public class DanceSchoolApplication {
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
+            SpringApplication.run(DanceSchoolApplication.class, args);
     }
 }
