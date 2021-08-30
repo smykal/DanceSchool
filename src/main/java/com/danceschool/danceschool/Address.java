5e233d1180a508eb99c1546bd0b8fc122905d2a5
Mateusz <smykal@engineer.com>
2021-08-30 19:07:28 +0200

    working connection to db

---
+++ src/main/java/com/danceschool/danceschool/Address.java
@@ -0,0 +1,9 @@
+package com.danceschool.danceschool;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+public class Address {
+}
--- src/main/java/com/danceschool/danceschool/repositories/SchoolRepositoryClass.java
+++ src/main/java/com/danceschool/danceschool/repositories/SchoolRepositoryClass.java
@@ -2,8 +2,10 @@ package com.danceschool.danceschool.repositories;
 
 import com.danceschool.danceschool.schools.School;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
-public interface SchoolRepository extends JpaRepository<School, UUID> {
+public interface SchoolRepositoryClass {
+
 }
